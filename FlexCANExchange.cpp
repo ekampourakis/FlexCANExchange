c0baefe825c6dc2c353cf99083d78d5974362d24
Manos Kampourakis <ekampourakis@ceid.upatras.gr>
2021-06-25 13:48:23 +0300

    minor updates

--- FlexCANExchange.cpp
+++ FlexCANExchange.cpp
@@ -1,10 +1,79 @@
 #include "FlexCANExchange.h"
 
+FlexCANExchange::FlexCANExchange() {}
+
+FlexCANExchange::FlexCANExchange(FlexCAN *CAN, uint8_t Containers) { Init(CAN, Containers); }
+
+FlexCANExchange::~FlexCANExchange() {
+    _MaxContainers = 0;
+    _Containers = nullptr;
+    _CAN = nullptr;
+}
+
 void FlexCANExchange::Init(FlexCAN *CAN, uint8_t Containers) {
-	_CAN = CAN;
-	_Containers = Containers;
+    _CAN = CAN;
+    _MaxContainers = Containers;
+    _Containers = (FlexCANExchangeContainer *)malloc(sizeof(FlexCANExchangeContainer) * Containers);
 }
 
-void FlexCANExchange::Handle(CAN_message_t *Message) {
-
+bool FlexCANExchange::Transmit(CAN_message_t Message) {
+    if (_CAN != nullptr) {
+        if (_CAN->write(Message)) {
+            return true;
+        }
+    }
+    return false;
 }
+
+int16_t FlexCANExchange::SearchContainers(uint32_t ID) {
+    for (uint8_t Container = 0; Container < _ActiveContainers; Container++) {
+        if (_Containers[Container].ResponseID == ID) {
+            return Container;
+        }
+    }
+    return -1;
+}
+
+int8_t FlexCANExchange::Request(
+    CAN_message_t TX, uint32_t ResponseID, uint8_t Retries, uint16_t Timeout, CAN_message_t *RX) {
+
+    // If it does overwrite it?
+    // Create the container and start initializing it
+    // Search if another with same ID exists
+    FlexCANExchangeContainer *Ptr;
+    if (const int16_t PresentContainer = SearchContainers(ResponseID) == -1) {
+        // Not found. Create a new container
+        Ptr = &_Containers[_ActiveContainers++];
+    } else {
+        // Already exists
+        Ptr = &_Containers[PresentContainer];
+    }
+    // Reset the container
+    *Ptr = FlexCANExchangeContainer();
+    Ptr->ResponseID = ResponseID;
+    Ptr->Timeout = Timeout;
+    Ptr->TriesLeft = Retries;
+    // Ptr->LastTry = 0;
+    Ptr->Request = TX;
+    Ptr->ResponsePtr = RX;
+
+
+    return -1;
+}
+
+bool FlexCANExchange::Handle(CAN_message_t *Message) {
+    if (_Containers != nullptr) {
+    }
+    return false;
+}
+
+// if (_CAN == nullptr || _Containers != nullptr && ) {
+// 	// Put it in the containers
+
+// 	if (!_CAN.write(Message)) {
+// 		// If the write wasn't successful, then add one more try
+// 	}
+// 	return true;
+// }
+
+// return 1;
--- FlexCANExchange.h
+++ FlexCANExchange.h
@@ -5,15 +5,37 @@
 #include <FlexCAN.h>
 
 class FlexCANExchange {
+    struct FlexCANExchangeContainer {
+        uint32_t ResponseID;
+        uint16_t Timeout;
+        uint8_t TriesLeft;
+        unsigned long LastTry;
+        bool Expired;
+        bool Success;
+        CAN_message_t Request;
+        CAN_message_t Response;
+        CAN_message_t *ResponsePtr = nullptr;
+    };
+
 public:
+    FlexCANExchange();
+    FlexCANExchange(FlexCAN *CAN, uint8_t Containers = 10);
+    ~FlexCANExchange();
     void Init(FlexCAN *CAN, uint8_t Containers = 10);
-    void Handle(CAN_message_t *Message);
-    void Request(const CAN_message_t *TX, const uint32_t ResponseID, CAN_message_t *RX = nullptr);
+    int8_t Request(CAN_message_t TX, uint32_t ResponseID, uint8_t Retries = 3,
+        uint16_t Timeout = 100, CAN_message_t *RX = nullptr);
+    bool Handle(CAN_message_t *Message);
+
     bool Available(bool Remove = true);
 
 private:
-    FlexCAN *_CAN;
-    uint8_t _Containers;
+    bool Transmit(CAN_message_t Message);
+    int16_t SearchContainers(uint32_t ID);
+
+    FlexCAN *_CAN = nullptr;
+    FlexCANExchangeContainer *_Containers = nullptr;
+    uint8_t _ActiveContainers;
+    uint8_t _MaxContainers;
 };
 
 #endif
